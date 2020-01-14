f57cc582b2a1c18e23c0cbf8bba7feeb04560b93
AndreyMakhhankov <andrey.makhankov@yandex.ru>
2020-01-14 15:03:39 +0300

    mydjangoapp,first commit

---
+++ try2ndlife/MYENV/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\8142~1\projects\try2nd~1\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
